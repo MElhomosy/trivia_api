83b67813810110bb28e349577fbee85d8a896a66
Mohammed ElHomosy <mohammed.elhomosy@aptiv.com>
2020-12-17 19:41:23 +0200

    commit 3
    create delete method

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -21,14 +21,13 @@ def create_app(test_config=None):
         return response
 
     @app.route('/categories')
-    def get_categories():
-        categories = Category.query.all()
+    def retrieve_categories():
+        categories = Category.query.order_by(Question.id).all()
         formated_categories = [category.format() for category in categories]
         return jsonify({
           'success':True,
           'categories':formated_categories
         })
-
 
     '''
     @TODO:
@@ -43,25 +42,36 @@ def create_app(test_config=None):
     Clicking on the page numbers should update the questions.
     '''
     @app.route('/questions')
-    def get_questions():
+    def retrieve_questions():
         page = request.args.get('page', 1, type=int)
-        start = (page-1) * 10
-        end = start + 10  
-        questions = Question.query.all()
+        start = (page-1) * QUESTIONS_PER_PAGE
+        end = start + QUESTIONS_PER_PAGE
+        questions = Question.query.order_by(Question.id).all()
         formated_questions = [question.format() for question in questions]
+        current_questions = formated_questions[start:end]
+        if len(current_questions) == 0:
+            abort(404)
+
         return jsonify({
           'success':True,
           'questions':formated_questions[start:end],
-          'total_questions': len(formated_questions)
+          'total_questions': len(formated_questions),
+          'categories': None,
+          'current_category': None
         })
 
-    '''
-    @TODO:
-    Create an endpoint to DELETE question using a question ID.
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    def delete_specific_question(question_id):
+        question = Question.query.filter(Question.id == question_id).one_or_none()
 
-    TEST: When you click the trash icon next to a question, the question will be removed.
-    This removal will persist in the database and when you refresh the page.
-    '''
+        if question is None:
+            abort(404)
+
+        question.delete()
+
+        return jsonify({
+          'success':True
+        })
 
     '''
     @TODO:
